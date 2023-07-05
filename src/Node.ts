610273c1c59d7b95955a966863adedbe3cf916a1
Jan Steikowski <j.steikowski@aol.com>
2023-07-05 15:49:47 +0700

    Add properties to NodeClass

--- src/Node.ts
+++ src/Node.ts
@@ -1,1 +1,10 @@
 "use strict";
+
+class LinkedListNode {
+  value: any;
+  next: LinkedListNode;
+  constructor(value: any = null, next: any = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
