17a997fcfd8735b70a8914242546de5238d644c0
Jan Steikowski <j.steikowski@aol.com>
2023-07-06 17:01:11 +0700

    delete typescript stuff

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { LinkedList } from './LinkedList';
-import { LinkedListNode } from './Node';
-
-const newList = new LinkedList(new LinkedListNode());
-console.log(newList);
-newList.append(42);
-console.log(newList);
